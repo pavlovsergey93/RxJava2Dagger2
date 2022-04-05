d337af648f9901ac2e6af5b6c870ad58324cd62e
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-04-05 17:18:29 +0300

    Создан макет ЛОГИН

--- app/src/main/java/com/gmail/pavlovsv93/rxjava2_dagger2/LoginActivity.kt
+++ app/src/main/java/com/gmail/pavlovsv93/rxjava2_dagger2/LoginActivity.kt
@@ -3,9 +3,12 @@ package com.gmail.pavlovsv93.rxjava2_dagger2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		setContentView(R.layout.activity_login)
+		supportFragmentManager.beginTransaction()
+			.replace(R.id.fragment_container, LoginFragment.newInstance())
+			.commit()
 	}
 }
---
+++ app/src/main/java/com/gmail/pavlovsv93/rxjava2_dagger2/LoginFragment.kt
@@ -0,0 +1,11 @@
+package com.gmail.pavlovsv93.rxjava2_dagger2
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment() {
+
+	companion object {
+		fun newInstance() = LoginFragment()
+	}
+
+}
