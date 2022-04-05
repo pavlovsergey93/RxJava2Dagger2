c8df0e1c6821e9b550b06adfe37017becb39d39e
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-04-05 16:50:28 +0300

    Начало модуля <<Популярные библиотеки>>

---
+++ app/src/main/java/com/gmail/pavlovsv93/rxjava2_dagger2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.pavlovsv93.rxjava2_dagger2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
